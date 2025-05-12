0bc0681dbc77e4b46761f15ac2f0c80f5212e4fe
Emily <emilyj4482@gmail.com>
2025-05-12 15:22:43 +0900

    [chore] create project

---
+++ SignInPage/Delegate/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  SignInPage
+//
+//  Created by EMILY on 12/05/2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.backgroundColor = .systemBackground
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
---
+++ SignInPage/View/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SignInPage
+//
+//  Created by EMILY on 12/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
