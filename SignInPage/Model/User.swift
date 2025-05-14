2ce0cb9f7c3bfc9ba938d58f3f6b70296b85d8cf
emilyj4482 <emilyj4482@gmail.com>
2025-05-14 22:30:36 +0900

    [feat] create User model

---
+++ SignInPage/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SignInPage
+//
+//  Created by EMILY on 14/05/2025.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let password: String
+    let nickname: String
+}
